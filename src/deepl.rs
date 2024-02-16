ba5af79bd343de97f63d789cb31c0782692770fe
oyoshot <oyoshot@gmail.com>
2024-02-17 02:27:21 +0900

    Remove attribute

--- src/deepl.rs
+++ src/deepl.rs
@@ -16,26 +16,24 @@ struct Translation {
     detected_source_language: String,
 }
 
-#[tokio::main]
-pub async fn deepl(endpoint: &str, api_key: &str, message: &str, lang: &str) -> Result<()> {
+pub fn deepl(endpoint: &str, api_key: &str, message: &str, lang: &str) -> Result<()> {
     let mut params = HashMap::new();
     params.insert("text", &message);
     params.insert("target_lang", &lang);
 
-    let client = reqwest::Client::new();
+    let client = reqwest::blocking::Client::new();
     let res = client
         .post(endpoint)
         .header(AUTHORIZATION, "DeepL-Auth-Key ".to_owned() + api_key)
         .header(CONTENT_TYPE, "json")
         .form(&params)
-        .send()
-        .await?;
+        .send()?;
 
     if !&res.status().is_success() {
         eprintln!("failed to request: {:?}", res);
     }
 
-    let p: DeeplResponse = serde_json::from_str(&res.text().await?)?;
+    let p: DeeplResponse = serde_json::from_str(&res.text()?)?;
     println!("\n{}", &p.translations[0].text);
 
     Ok(())
